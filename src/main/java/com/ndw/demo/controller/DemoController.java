9e53c9833e5c1cf2b8da9205b9fdcbfcaba93ca0
wdn <93695684@qq.com>
2019-08-07 16:07:20 +0800

    测试 第一次用idea来git项目

---
+++ src/main/java/com/ndw/demo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.ndw.demo.controller;
+
+import com.ndw.demo.entity.DemoModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/getDemo")
+    public DemoModel getDemo(){
+        DemoModel dm=new DemoModel();
+        dm.setId(1);
+        dm.setName("王大年");
+        dm.setMoble("13126796948");
+        return dm;
+    }
+
+}
---
+++ src/main/java/com/ndw/demo/entity/DemoModel.java
@@ -0,0 +1,10 @@
+package com.ndw.demo.entity;
+
+import lombok.Data;
+
+@Data
+public class DemoModel {
+    private Integer id;
+    private String name;
+    private String moble;
+}
